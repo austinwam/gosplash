3d33634570f1a53b84b3423b6ff8880b47e7d1fd
austinwam <mutonyiaustin@gmail.com>
2023-02-24 10:05:20 +0300

    init push

---
+++ lib/pages.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Splashpage extends StatelessWidget {
+  const Splashpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
